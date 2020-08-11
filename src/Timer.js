5ce3dc4fc4e8d7eca4d85ccf523e5633a349c64a
Drew Beckmen <drew.beckmen@yale.edu>
2020-08-11 13:51:13 -0400

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -8,6 +8,19 @@ class Timer extends Component {
 
   // add your code here
 
+  //good place to set up interval/timeout functions and fetch data
+  //anything asynchronous in componentDidMount()
+  componentDidMount() {
+    //assign interval variable within scope of our class
+    this.interval = setInterval(this.clockTick, 1000)
+  }
+
+  //clean up after ourselves: get rid of any intervals
+  //that were set up in componentDidMount()
+  componentWillUnmount() {
+    clearInterval(this.interval) //built-in method that you can use
+  }
+
   render() {
     const { time, color } = this.state;
     return (
@@ -31,7 +44,7 @@ class Timer extends Component {
     clearInterval(this.interval);
   };
 
-  // for the 'x' button,
+  // for the 'x' button, passing a function as props from the parent component.
   handleClose = () => {
     this.props.removeTimer(this.props.id);
   };
